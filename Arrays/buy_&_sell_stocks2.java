ee29acece93a80e99907a44652eb9439d72d281c
PRAJAKTAC12 <141652156+PRAJAKTAC12@users.noreply.github.com>
2024-07-23 13:14:47 +0530

    buy_&_sell_stocks2.java

---
+++ Arrays/buy_&_sell_stocks2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        for( int i=0;i<prices.length;i++){
+            if(i<prices.length-1){
+                if(prices[i]>prices[i+1]){
+                     continue;
+                }
+                else{
+                    profit += prices[i+1] - prices[i];
+                 }
+            }
+               
+        }
+        return profit;
+    }
+}
